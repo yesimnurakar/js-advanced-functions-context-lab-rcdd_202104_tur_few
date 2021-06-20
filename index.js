0836573ab409ac116aef85ed30e417fc0fca3ff6
Yesim nur Akar <yesimnurakar@gmail.com>
2021-06-20 14:40:06 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -8,11 +8,34 @@ let createEmployeeRecord = function(row){
         timeOutEvents: []
     }
 }
+let createEmployeeRecords = function(employeeRowData) {
+    return employeeRowData.map(function(row){
+        return createEmployeeRecord(row)
+    })
+}
+
+let createTimeInEvent = function(dateStamp){
+    let [date, hour] = dateStamp.split(' ')
+
+    this.timeInEvents.push({
+        type: "TimeIn",
+        hour: parseInt(hour, 10),
+        date,
+    })
+
     return this
 }
 
-let allWagesFor = function () {
-    let eligibleDates = this.timeInEvents.map(function (e) {
+let createTimeOutEvent = function(dateStamp){
+    let [date, hour] = dateStamp.split(' ')
+
+    this.timeOutEvents.push({
+        type: "TimeOut",
+        hour: parseInt(hour, 10),
+        date,
+    })
+    return this
+}
 let hoursWorkedOnDate = function(soughtDate){
     let inEvent = this.timeInEvents.find(function(e){
         return e.date === soughtDate
@@ -36,22 +59,21 @@ let allWagesFor = function(){
         return e.date
     })
 
-    let payable = eligibleDates.reduce(function (memo, d) {
     let payable = eligibleDates.reduce(function(memo, d){
-        return memo + wagesEarnedOnDate.call(this, d)
-    }.bind(this), 0)
-    }.bind(this), 0)
+       return memo + wagesEarnedOnDate.call(this, d)
+   }.bind(this), 0)
 
-    return payable
+   return payable
 }
 
 let findEmployeeByFirstName = function(srcArray, firstName) {
-  return srcArray.find(function(rec){
-    return rec.firstName === firstName
-  })
+ return srcArray.find(function(rec){
+   return rec.firstName === firstName
+ })
 }
 
 let calculatePayroll = function(arrayOfEmployeeRecords){
-    return arrayOfEmployeeRecords.reduce(function(memo, rec){
-        return memo + allWagesFor.call(rec)
-    }, 0)
+   return arrayOfEmployeeRecords.reduce(function(memo, rec){
+       return memo + allWagesFor.call(rec)
+   }, 0)
+ }
